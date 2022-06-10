73039afa31fe95af7863c321e0cda20b0b147eb2
Abdullah Mohamed <abdullahabdin@live.com>
2022-06-10 23:39:10 +0100

    API Fetch done + fixed stacking images

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ let randomGenerate_btn = document.querySelector(".randomGenerate_btn");
 
 randomGenerate_btn.addEventListener('click', fetchRandomCats);
 
-
-
 function fetchRandomCats(){
     fetch('https://api.thecatapi.com/v1/images/search') 
     .then(response => response.json())
     .then((data) => {
+       
         let catImageUrl = data[0].url
 
         let catElement = document.createElement("img")
         catElement.setAttribute('src',`${catImageUrl}`)
+        catElement.classList.add('catContainer')
+
         let catGenerate = document.querySelector(".catGenerate")
         catGenerate.appendChild(catElement)
     });
+
+    let catGenerate = document.querySelector(".catGenerate")
+    catGenerate.innerHTML = ''
+
 }
